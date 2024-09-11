1b6ac3a171588f486812be110348c9ad87cfa22e
Matteo VIllani <villanimatteo33@gmail.com>
2024-09-11 20:19:52 +0200

    Random colors for pie chart generator

--- assets/js/lastUpdate.js
+++ assets/js/lastUpdate.js
@@ -32,4 +32,4 @@ function displayLastUpdate(dateString) {
 }
 
 // Call the function with the last update date in dd/mm/yyyy format
-displayLastUpdate('10/9/2024'); // TODO: REPLACE
+displayLastUpdate('11/9/2024'); // TODO: REPLACE
